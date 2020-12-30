82f80eb5732b04c999271e6622ea6beda7f9ff78
JackKalina <jojok4@comcast.net>
2020-12-29 19:37:18 -0500

    Fish command fixed
    Turns out last time the fish function was actually busted. It double-fished every time it was first used by a user and occasionally didn't save data. This is now fixed.

--- commands/fish.js
+++ commands/fish.js
@@ -19,25 +19,26 @@ module.exports = {
             num *= max - min;
             num += min;
             num = Math.floor(num);
-            console.log(`Generated number: ${num}`);
             return num;
         }
         function convertTime(ms){
             let seconds = ms / 1000;
-            let hours = parseInt(seconds/3600);
-            seconds = seconds % 3600;
+            //let hours = parseInt(seconds/3600);
+            //seconds = seconds % 3600;
             let minutes = parseInt(seconds/60); 
             seconds = seconds % 60;
             seconds = Math.floor(seconds);
-            return (`${hours} hours ${minutes} minutes ${seconds} seconds`);
+            return (`${minutes} minutes ${seconds} seconds`);
         }
         function fish(){
             numFish = generateNumber(0, 1000, 3.5);
-            newFishTotal = userFish + numFish;
-            db.collection(`${message.guild.id}`).doc(`${message.member.id}`).update({
+            newFishTotal = userFish + numFish; // Generates a number and adds it to user's current number of fish
+            db.collection(`${message.guild.id}`).doc(`${message.member.id}`).set({
                 'totalFish': newFishTotal,
-                'lastFishTime': timeAtCall
-            });
+                'lastFishTime': timeAtCall,
+                'name': message.member.user.username,
+                'nickname': message.member.nickname
+            }); // Sets data in db
             message.channel.send(`You caught **${numFish}** fish!`); 
         }
         // This generates 100 random numbers in order to help check distribution. Uncomment it when you need to tweak balance.
@@ -51,22 +52,20 @@ module.exports = {
         let newFishTotal;
         let userFishTime;
         let timeAtCall = Date.now();
-        db.collection(`${message.guild.id}`).doc(`${message.member.id}`).get().then((q) => {
-            if (q.exists) {
+        db.collection(`${message.guild.id}`).doc(`${message.member.id}`).get().then((q) => { // Checking if the user has data in Firestore
+            if (q.exists) { // If they do, pull it
                 userFish = q.data().totalFish;
                 userFishTime = q.data().lastFishTime;
-            } else {
-                db.collection(`${message.guild.id}`).doc(`${message.member.id}`).set({
-                    'totalFish': 0,
-                    'lastFishTime': 1
-                })
+            } else { // Else, set the userFish to 0 and call fish function
+                userFish = 0;
+                userFishTime = 1;
             }
         }).then(() => {
-            if (timeAtCall - userFishTime > 7200000 || userFishTime === 1) {
+            if (timeAtCall - userFishTime > 3600000 || userFishTime === 1) {
                 fish();
             } else {
                 let timeDiff = timeAtCall - userFishTime; 
-                message.channel.send(`Slow down! You can fish again in ${convertTime(7200000 - timeDiff)}.`);
+                message.channel.send(`Slow down! You can fish again in ${convertTime(3600000 - timeDiff)}.`);
             }
         });
         
