1fb5d74f5b379fe2c2e19a6dec6b7128affb293f
JackKalina <jojok4@comcast.net>
2020-12-29 22:18:21 -0500

    Made a totalfish command

---
+++ commands/totalfish.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'totalfish',
+    description: "Gets the user's total fish",
+    execute(message, args, db) {
+        let userFish;
+        db.collection(`${message.guild.id}`).doc(`${message.member.id}`).get().then((q) =>{
+            if (!q.exists){
+                message.channel.send("You need to fish first! Use the fish command.");
+            } else {
+                userFish = q.data().totalFish;
+                if (message.member.nickname != null){
+                    message.channel.send(`${message.member.user.username}, you have caught ${userFish} fish!`);
+                } else {
+                    message.channel.send(`${message.member.nickname}, you have caught ${userFish} fish!`);
+                }
+            }
+        })
+    }
+}
